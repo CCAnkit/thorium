33540701a5d9501801880731fb72a5ad41c9aa29
CCAnkit <ankitgupta9828@gmail.com>
2022-02-26 19:21:08 +0530

    MongoDB1

---
+++ src/models/userModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema( {
+    bookName: {
+        type: String,
+        required: true        
+    },
+    authorName:{
+        type: String,
+        required: true
+    },
+    category: String,
+    year: Number,
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema) //users
+
+// String, Number
+// Boolean, Object/json, array
