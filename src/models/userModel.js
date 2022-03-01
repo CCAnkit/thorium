bd0eee19aca8f0ad97222c0aaaf263c099e977ea
CCAnkit <ankitgupta9828@gmail.com>
2022-03-02 00:49:13 +0530

    Mongo2

---
+++ src/models/userModel.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema( {
+    firstName: String,
+    lastName: String,
+    mobile: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema) //users
+
+
+
+// String, Number
+// Boolean, Object/json, array
+
+
+// emailId: String,
+//     gender: {
+//         type: String,
+//         enum: ["male", "female", "LGBTQ"] //"falana" will give an error
+//     },
+//     age: Number,
+//     // isIndian: Boolean,
+//     // parentsInfo: {
+//     //     motherName: String,
+//     //     fatherName: String,
+//     //     siblingName: String
+//     // },
+//     // cars: [ String  ]
---
+++ src/routes/route.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const Userbook= require("../models/bookModel.js")
+const bookController= require("../controllers/bookController")
+
+router.get("/test-me", function (req, res) {
+    res.send("My first ever api!")
+})
+
+router.post("/createBook", bookController.createBook)
+
+router.get("/getBooksData", bookController.bookList)
+
+router.get("/bookYearVise", bookController.getBooksInYear)
+
+router.get("/getXINRBooks", bookController.getXINRBooks)
+
+router.get("/getRandomBooks", bookController.getRandomBooks)
+
+router.get("/getParticularBooks", bookController.getParticularBooks)
+
+module.exports = router;
