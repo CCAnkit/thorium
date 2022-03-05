69d48966b6aac92b9dcb01beded1c959c84b5f06
CCAnkit <ankitgupta9828@gmail.com>
2022-03-06 01:35:28 +0530

    middleware

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const requestIp = require('request-ip');
+const route = require('./routes/route.js');
+const { default: mongoose } = require('mongoose');
+const moment = require('moment');
+
+
+const app = express();
+
+
+app.use(requestIp.mw({attributeName:'myIp'}))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.use(function(req,res,next){
+   
+  let newData=moment().format('YYYY-MM-DD HH:mm:ss:a');
+  let ip=req.myIp;
+  let apiMy=req.originalUrl;
+  console.log(newData,ip,apiMy)
+    next()
+})
+
+mongoose.connect("mongodb+srv://CCAnkit:CCAnkit09@clusternew.gds9x.mongodb.net/Ankit", {
+    useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch ( err => console.log(err) )
+
+app.use('/', route);
+
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
---
+++ src/routes/route.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+// const UserModel= require("../models/userModel.js")
+const UserController= require("../controllers/userController")
+const BookController= require("../controllers/bookController")
+
+router.get("/test-me", function (req, res) {
+    res.send("My first ever api!")
+})
+
+router.get("/demoApi", function (req, res){
+    res.send("such cool api")
+})
+
+router.post("/falanaApi", function (req,res){
+    res.send("dinkhana")
+})
+
+
+module.exports = router;
